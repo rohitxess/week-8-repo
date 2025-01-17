13a9e92347f43d0d3a979044ae8fec27cb30df07
rohitxess <xessro@gmail.com>
2025-01-17 16:35:46 +0530

    exported npm packages and changed json file

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -7,11 +7,11 @@ import { z } from "zod";
 
 const router = express.Router();
 
-const signupInput = z.object({
-  username: z.string(),
-  password: z.string(),
-})
-
+// const signupInput = z.object({
+//   username: z.string(),
+//   password: z.string(),     
+// })
+            
 type SignupParams = z.infer<typeof signupInput>;
 
 router.post('/signup', async (req, res) => {
